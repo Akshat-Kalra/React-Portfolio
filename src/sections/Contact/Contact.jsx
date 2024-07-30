635e43149d055056f13e8929b7959728372ac1c9
akshatkalra <akalra06@students.cs.ubc.ca>
2024-07-31 02:31:49 +0530

    added email to contact

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -6,6 +6,7 @@ import githubLight from '../../assets/github-light.svg'
 import githubDark from '../../assets/github-dark.svg'
 import linkedInLight from '../../assets/linkedin-light.svg'
 import linkedInDark from '../../assets/linkedin-dark.svg'
+import emailImg from '../../assets/email.svg'
 
 
 
@@ -16,19 +17,19 @@ function Contact() {
         <h1 className='sectionTitle'>CONTACT</h1>
         <div className={styles.parentContact}>
         <div className={styles.contactLinks}>
-        <a href='https://github.com' target='_blank' className={styles.projectCard}>
+        <a href='https://github.com/akshat-kalra' target='_blank' className={styles.projectCard}>
             <img src={githubLight} />
             <span>Github</span>
         </a>
 
-        <a href='https://linkedin.com' target='_blank' className={styles.projectCard}>
+        <a href='https://www.linkedin.com/in/akshat-kalra-783819156/' target='_blank' className={styles.projectCard}>
             <img src={linkedInLight} />
             <span>LinkedIn</span>
         </a>
 
-        <a href='https://linkedin.com' target='_blank' className={styles.projectCard}>
-            <img src={linkedInLight} />
-            <span>LinkedIn</span>
+        <a href='mailto:akshatkalra2005@gmail.com' target='_blank' className={styles.projectCard}>
+            <img src={emailImg} />
+            <span>Email Id</span>
         </a>
         </div>
         </div>
