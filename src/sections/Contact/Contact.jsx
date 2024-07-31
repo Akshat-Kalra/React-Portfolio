7d716da849ea4d3eaf4760e694863efea6131062
akshatkalra <akalra06@students.cs.ubc.ca>
2024-07-31 14:35:40 +0530

    added projects

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
+// import React from 'react'
 import styles from './ContactStyles.module.css'
-import twitterLight from '../../assets/twitter-light.svg'
-import twitterDark from '../../assets/twitter-dark.svg'
 import githubLight from '../../assets/github-light.svg'
-import githubDark from '../../assets/github-dark.svg'
+// import githubDark from '../../assets/github-dark.svg'
 import linkedInLight from '../../assets/linkedin-light.svg'
-import linkedInDark from '../../assets/linkedin-dark.svg'
+// import linkedInDark from '../../assets/linkedin-dark.svg'
 import emailImg from '../../assets/email.svg'
 
 
--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+// import React from 'react'
 import styles from './ProjectsStyles.module.css'
-import viberr from '../../assets/viberr.png'
+import titanic from '../../assets/titanic.png'
+import cat from '../../assets/cat.png'
+import dumbell from '../../assets/dumbell.png'
 
 function Projects() {
   return (
     <section className={styles.container}>
         <h1 className='sectionTitle'>PROJECTS</h1>
         <div className={styles.projectsContainer}>
-        <Project img={viberr} name="FitTrackr" description="blah blah blah blah blah " link="https://github.com/Akshat-Kalra/FitTrackr"/>
-        <Project img={viberr} name="App 1" description="blah blah blah blah blah "/>
-        <Project img={viberr} name="App 1" description="blah blah blah blah blah "/>
+        <Project img={dumbell} name="FitTrackr" description='Java-based fitness tracking app. Logs workouts, visualizes progress, and persists data.' link="https://github.com/Akshat-Kalra/FitTrackr"/>
+        <Project img={cat} name="Only Cat Facts" description="ReactJS-powered cat facts app. Fetches data from APIs, displays cute cat content." link="https://onlycatfacts.netlify.app/"/>
+        <Project img={titanic} name="Inferential Analysis" description="Statistical Inference on whether Ticket Classes affected Survival Rates on the RMS Titanic?" link="https://github.com/Akshat-Kalra/STAT_201_PROJECT"/>
         </div>
     </section>
   )
@@ -21,11 +23,14 @@ function Project(props) {
     const description = props.description
     const link = props.link
     return (
-        <a href={link} target='_blank' className={styles.projectCard}>
+        <div className={styles.projectCardParent}>
+            <a href={link} target='_blank' className={styles.projectCard}>
             <img src={img} />
             <h3>{name}</h3>
             <p>{description}</p>
         </a>
+        </div>
+        
     )
 }
 
