18d67e180cec9a765384db1a854d1e8d8cc48a27
akshatkalra <akalra06@students.cs.ubc.ca>
2024-08-01 19:40:08 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,25 @@ import Hero from './sections/Hero/Hero'
 import Projects from './sections/Projects/Projects'
 import Skills from './sections/Skills/Skills'
 import Contact from './sections/Contact/Contact'
+import Navbar from './sections/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
-    <Hero />
-    <Projects />
-    <Skills />
-    <Contact />
+    <Navbar />
+    <div id="hero">
+        <Hero />
+      </div>
+      <div id="projects">
+        <Projects />
+      </div>
+      <div id="skills">
+        <Skills />
+      </div>
+      <div id="contact">
+        <Contact />
+      </div>
     </>
   )
 }
