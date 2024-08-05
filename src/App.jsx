5bc6f26af0e0b1f0c888326689e3bd32e7c3aaf1
akshatkalra <akshatkalra2005@gmail.com>
2024-08-05 17:06:12 +0530

    added vercel analytics component

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Projects from "./sections/Projects/Projects";
 import Skills from "./sections/Skills/Skills";
 import Contact from "./sections/Contact/Contact";
 import Navbar from "./sections/Navbar/Navbar";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div id="nav">
+      <Analytics />
       <Navbar />
       <div className="content">
         <div id="hero">
